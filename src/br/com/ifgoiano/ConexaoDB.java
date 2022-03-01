8ed952899e90fdb3b8708f28cdacbbbc2deeada7
MarcosPauloSR <marcospaulosrezende@gmail.com>
2022-03-01 18:10:28 -0300

    conexão com o banco de dados criada e com metodos para select, create, update e delete

--- src/br/com/ifgoiano/ConexaoDB.java
+++ src/br/com/ifgoiano/ConexaoDB.java
@@ -2,6 +2,8 @@ package br.com.ifgoiano;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ConexaoDB {
 
@@ -22,6 +24,31 @@ public class ConexaoDB {
 
         } catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    //Método para fazer INSERT, UPDATE OU DELETE
+    public int executaSql(String sql){
+        try{
+            Statement stm = conector.createStatement();
+            int res = stm.executeUpdate(sql);
+            conector.close();
+            return res;
+        } catch (Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    //Método para fazer select no banco
+    public ResultSet buscaDados(String sql){
+        try {
+            Statement stm = conector.createStatement();
+            ResultSet res = stm.executeQuery(sql);
+            conector.close();
+            return res;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
         }
     }
 
--- src/br/com/ifgoiano/Main.java
+++ src/br/com/ifgoiano/Main.java
@@ -1,10 +1,27 @@
 package br.com.ifgoiano;
 
+import java.sql.ResultSet;
+
 public class Main {
 
     public static void main(String[] args) {
 
         ConexaoDB con = new ConexaoDB();
+        String sql = "SELECT * FROM usuarios";
+
+        ResultSet rs = con.buscaDados(sql);
+
+        try {
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String email = rs.getString("email");
+                System.out.println("Id: " + id + " | Nome: " + nome + " | Email: " + email);
+                System.out.println("==================================");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
 
